33443fdcd102a681fe2e3aa6ad17184fea93c165
Daniel Perdomo <136826084+GoblinSwarm@users.noreply.github.com>
2024-06-25 22:42:50 +0000

    Add functionality: add task, delete task, message

--- src/views/Home.jsx
+++ src/views/Home.jsx
@@ -9,34 +9,70 @@ const initialState = {
 const Home = () => {
 
 	const [task, setTask] = useState(initialState)
-	const [taskList, setTaskList] = useState([
-		{
-			"task": "Wash the dishes."
-		},
-		{
-			"task": "Walk the dog."
+	const [taskList, setTaskList] = useState([])	
+
+
+	const handleChange = (event) => {
+		setTask({
+			...task,
+			[event.target.name]: event.target.value
+		})
+	}
+	
+	const handleAddTask = () => {
+		if (task.task.trim() !== "") {
+			setTaskList([task, ...taskList]);
+			setTask(initialState);
 		}
-	])	
+	}
+
+	const handleKeyPress = (event) => {
+		if (event.key === "Enter") {
+		  handleAddTask();
+		}
+	};
+
+	const handleTaskClick = (index) => {
+		const newTaskList = [...taskList];
+		newTaskList.splice(index, 1)
+		setTaskList(newTaskList)
+	};
 
 	return (
-		<div className="text-center">
+		<div className="text-center" style={{backgroundColor: "grey"}}>
 			<div className="container text-center mb-1">
-				<h1>To DOS</h1>
-  				<label for="form-text" class="form-label"></label>
-  				<textarea className="form-control" id="exampleFormControlTextarea1" rows="3"></textarea>
+				<h1>To-Do-List</h1>
+  				<label htmlFor="form-text" className="form-label"></label>
+  				<input
+								type="text"
+								className="form-control"
+								id="txtTask"
+								placeholder="Car washing"
+								name="task"
+								value={task.task}
+								onChange={handleChange}
+								onKeyDown={handleKeyPress}
+				/>
 			</div>
 			<div>
+				{taskList.length === 0 ? (
+          		<p className="text-center mt-3">No tasks added yet.</p>
+        		) : (
 				<ul className="list-group">
 						{
 							taskList.map((item, index) => {
 								return (
-									<li key={index}>
-										<p>{item.task}</p>
+									<li key={index}
+										onClick={() => handleTaskClick(index)}
+										style={{ cursor: 'pointer' }}>
+										<p>{item.task} <i className="fas fa-window-close"></i></p>
+										
 									</li>
 								)
 							})
 						}
 				</ul>
+				)}
 			</div>
 		</div>
 	);
